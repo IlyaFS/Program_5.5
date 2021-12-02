e154cafe893ac0364d1e61899296d131f82d4f29
Илья Филиппов <filippov.i.s@gmail.com>
2021-12-03 02:16:24 +0300

    Добавьте файлы проекта.

---
+++ Program_5.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите целое число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+           
+            int[,] mas = new int [N,N];
+            for (int i = 0; i <N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    mas[i, j]= 1 - (i + j) % 2;
+                    Console.Write("{0} ", mas[i,j]);                    
+                }
+                Console.WriteLine();                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Для завершения нажмите на любого клавиша");
+            Console.ReadKey();
+        }
+    }
+}
